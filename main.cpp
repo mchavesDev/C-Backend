93cffa294343c396e6d0a4610cade379aab49f04
Mario Chaves <mchavessantisteban@gmail.com>
2026-01-07 14:52:30 +0100

    class http created

--- main.cpp
+++ main.cpp
@@ -6,32 +6,40 @@
 #include <arpa/inet.h>
 #include <thread>
 #include <vector>
-#include <string>
+#include <sstream>
+#include "src/http.cpp"
 
 constexpr int MAX_EVENTS = 10;
 constexpr int MAX_CLIENTS = 10;
 constexpr int PORT = 8080;
 using namespace std;
 
-
 // Function to handle client connections in a separate thread
-void handleClient(int clientFd) {
+void handleClient(int clientFd)
+{
     char buffer[1024] = "";
+    std::stringstream response;
 
-    while (true) {
+    while (true)
+    {
         int bytesRead = read(clientFd, buffer, sizeof(buffer));
-        if (bytesRead <= 0) {
+        if (bytesRead <= 0)
+        {
             std::cout << buffer << std::endl;
+            httpRequest request;
+            request.setRequest("GET / HTTP/1.1\r\n");
+
             break;
         }
-        write(clientFd, buffer, bytesRead);
-        
-        
+        response << "HTTP/1.1 200 OK\r\n";
+        response << "Content-Length: 46\r\n"; // Length of the HTML content
+        response << "Content-Type: text/html\r\n";
+        response << "\r\n";
+        response << "<html><body><h1>Hello, World!</h1></body></html>";
+        write(clientFd, response.str().c_str(), response.str().length());
     }
-    
     close(clientFd);
 }
-
 
 int main()
 {
@@ -39,32 +47,31 @@ int main()
     struct epoll_event event, events[MAX_EVENTS];
     struct sockaddr_in serverAddress;
     serverFd = socket(AF_INET, SOCK_STREAM, 0);
-    if(serverFd == -1)
+    if (serverFd == -1)
     {
         std::cerr << "Failed to create socket" << std::endl;
         return 1;
     }
-    
-    
+
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(PORT);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-    if(bind(serverFd, (struct sockaddr*)&serverAddress, sizeof(serverAddress)))
+    if (bind(serverFd, (struct sockaddr *)&serverAddress, sizeof(serverAddress)))
     {
         std::cerr << "Failed to bind socket" << std::endl;
         close(serverFd);
         return 1;
     }
-    if(listen(serverFd, MAX_CLIENTS) == -1)
+    if (listen(serverFd, MAX_CLIENTS) == -1)
     {
-         std::cerr << "Failed to listen socket" << std::endl;
+        std::cerr << "Failed to listen socket" << std::endl;
         close(serverFd);
         return 1;
     }
 
-    //Create epoll instance
-    
+    // Create epoll instance
+
     epollFd = epoll_create1(0);
     if (epollFd == -1)
     {
@@ -72,39 +79,36 @@ int main()
         close(serverFd);
         return 1;
     }
-    
+
     // Add server socket to epoll
 
     event.events = EPOLLIN;
     event.data.fd = serverFd;
-    if (epoll_ctl(epollFd, EPOLL_CTL_ADD, serverFd,&event)== -1)
+    if (epoll_ctl(epollFd, EPOLL_CTL_ADD, serverFd, &event) == -1)
     {
         std::cerr << "Failed to add server socket to epoll instance" << std::endl;
         close(serverFd);
         close(epollFd);
         return 1;
     }
-    
-
-
 
     std::cout << "Server started, listening on port " << PORT << std::endl;
-    while(true)
+    while (true)
     {
-        int numEvents = epoll_wait(epollFd,events,MAX_EVENTS, -1);
-        if(numEvents == -1)
+        int numEvents = epoll_wait(epollFd, events, MAX_EVENTS, -1);
+        if (numEvents == -1)
         {
             std::cerr << "Failed to wait for events" << std::endl;
             break;
         }
-        for (int i = 0; i < numEvents; i++ )
+        for (int i = 0; i < numEvents; i++)
         {
             if (events[i].data.fd == serverFd)
             {
                 // accept new client
                 struct sockaddr_in clientAddress;
                 socklen_t clientAddrressLength = sizeof(clientAddress);
-                int clientFd = accept(serverFd ,(struct sockaddr*)&clientAddress,&clientAddrressLength);
+                int clientFd = accept(serverFd, (struct sockaddr *)&clientAddress, &clientAddrressLength);
                 if (clientFd == -1)
                 {
                     std::cerr << "Failed to accept client connection" << std::endl;
@@ -123,7 +127,7 @@ int main()
                 std::thread clientThread(handleClient, clientFd);
                 clientThread.detach();
             }
-            else 
+            else
             {
                 // Handle client data
                 int clientFd = events[i].data.fd;
